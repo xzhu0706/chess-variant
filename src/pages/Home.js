03a3485e872c1dc03783fd303019479cf8d49364
cchen008 <cchen008@citymail.cuny.edu>
2019-11-03 13:07:36 -0500

    Added Welcome Description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,8 +9,26 @@ class Home extends Component {
   }
 
   render() {
+    const descriptionList = [
+      { description: 'Design your own variants', key: 0 },
+      { description: 'Test your variant against others', key: 1 },
+      { description: 'Play variants created from other players', key: 2 },
+    ];
+
     return (
-      <div>Home Page</div>
+      <div>
+        <div>
+          <a href="/">Chess-Variant.com</a>
+        </div>
+        <div>
+          <h1>Welcome to Chess-Variants</h1>
+          <ul>
+            {descriptionList.map(description => {
+              return (<li key={description.key}>{description.description}</li>);
+            })}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
