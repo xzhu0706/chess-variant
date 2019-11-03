38193ca19b7ecb0cb3c4166a330bec965b10a9b9
cchen008 <cchen008@citymail.cuny.edu>
2019-11-03 14:19:05 -0500

    Added Image/logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Column } from 'react-foundation';
 
 class Home extends Component {
   constructor(props) {
@@ -9,11 +10,16 @@ class Home extends Component {
   }
 
   render() {
-    const descriptionList = [
+    const descripList = [
       { description: 'Design your own variants', key: 0 },
       { description: 'Test your variant against others', key: 1 },
       { description: 'Play variants created from other players', key: 2 },
     ];
+
+    const imgStyle = {
+      width: '10em',
+      height: '10em',
+    };
 
     return (
       <div>
@@ -21,12 +27,19 @@ class Home extends Component {
           <a href="/">Chess-Variant.com</a>
         </div>
         <div>
-          <h1>Welcome to Chess-Variants</h1>
-          <ul>
-            {descriptionList.map(description => {
-              return (<li key={description.key}>{description.description}</li>);
-            })}
-          </ul>
+          <Row>
+            <Column>
+              <div className="col">
+                <h1>Welcome to Chess-Variants</h1>
+                <ul>
+                  {descripList.map((description) => (<li key={description.key}>{description.description}</li>))}
+                </ul>
+              </div>
+            </Column>
+            <Column>
+              <img src="https://cdn0.iconfinder.com/data/icons/sports-and-games-4/512/172-512.png" alt="Chess Piece" style={imgStyle} />
+            </Column>
+          </Row>
         </div>
       </div>
     );
