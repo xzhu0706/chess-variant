31b2c4e51ee3ce8ad6d1deb76971c690fdfbdfa4
cchen008 <cchen008@citymail.cuny.edu>
2019-11-10 15:11:25 -0500

    Added Variant documentation page

---
+++ src/pages/VariantDocumentation.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class VariantDocumentation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+            hello
+      </div>
+    );
+  }
+}
+
+export default VariantDocumentation;
