b34a1083f3f085de1c01f41b8a6f719950df1ffe
cchen008 <cchen008@citymail.cuny.edu>
2019-11-10 15:12:41 -0500

    Added Variant Documentation path to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
+import VariantDocumentation from './pages/VariantDocumentation';
 import './App.css';
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/game/:token" component={Game} />
+          <Route path="/variants" component={VariantDocumentation} />
         </Switch>
       </Router>
     );
