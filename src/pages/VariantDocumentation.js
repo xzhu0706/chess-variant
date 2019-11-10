75a0dabed060e2074f9eb44e69d90d3fba41e8f6
cchen008 <cchen008@citymail.cuny.edu>
2019-11-10 16:09:23 -0500

    Added tabs for variant documentation

--- src/pages/VariantDocumentation.js
+++ src/pages/VariantDocumentation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import VariantNav from '../components/VariantNav';
 
 class VariantDocumentation extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class VariantDocumentation extends Component {
   render() {
     return (
       <div>
-            hello
+        <VariantNav />
       </div>
     );
   }
