dfe2186f8352a55ad0dc56dd351fa13577888ee1
Daniel Vignoles <dvignoles@gmail.com>
2019-11-11 23:19:24 -0500

    add profile img and account info

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Amplify, {Auth} from 'aws-amplify'
+import {
+    Row, Col, Image, ListGroup, ListGroupItem
+} from 'react-bootstrap'
+
 
 export default class Account extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            username: 'chessplayer',
+            email: 'example@email.com',
+            phone: '111-111-1111'
+        }
+    };
 
     async componentDidMount() {
         const user = await Auth.currentUserInfo();
         if (user) {
           this.setState({
-            user: user,
             username: user.username,
             email: user.attributes.email,
             email_verified: user.attributes.email_verified,
@@ -19,10 +30,28 @@ export default class Account extends Component {
 
     render() {
         return (
-            <div>
-                Account Page
-            </div>
+            <Profile 
+                username={this.state.username}
+                email={this.state.email}
+                phone={this.state.phone_number}
+            />
         )
     }
 }
 
+const Profile = (props) => (
+        <div>
+                <Row>
+                    <Col sm={{ span: 2, offset: 2}}>
+                        <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/ChessSet.jpg/250px-ChessSet.jpg' thumbnail />
+                        <ListGroup>
+                            <ListGroupItem variant="flush">{props.username}</ListGroupItem>
+                            <ListGroupItem>{props.email}</ListGroupItem>
+                            <ListGroupItem>{props.phone}</ListGroupItem>
+                        </ListGroup>
+                    </Col>
+                    <Col sm= {{ span: 6}}>
+                    </Col>
+                </Row>
+        </div>
+    )
