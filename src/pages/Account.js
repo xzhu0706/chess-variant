7bf73a41537b52677d8cb0b36e209ba1dc864525
Daniel Vignoles <dvignoles@gmail.com>
2019-11-12 00:54:09 -0500

    get Auth info as promise

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Amplify, {Auth} from 'aws-amplify'
 import {
-    Row, Col, Image, ListGroup, ListGroupItem, Table
+    Container, Row, Col, Image, ListGroup, ListGroupItem, Table
 } from 'react-bootstrap'
 
 
@@ -9,32 +9,27 @@ export default class Account extends Component {
     constructor(props){
         super(props)
         this.state ={
-            username: 'chessplayer',
-            email: 'example@email.com',
-            phone: '111-111-1111'
+
         }
     };
 
-    async componentDidMount() {
-        const user = await Auth.currentUserInfo();
-        if (user) {
-          this.setState({
-            username: user.username,
-            email: user.attributes.email,
-            email_verified: user.attributes.email_verified,
-            phone_number: user.attributes.phone_number,
-            phone_verified: user.attributes.phone_number_verified
-          });
-        }
+    componentDidMount() {
+        const user = Auth.currentUserInfo()
+        user.then((result) => {
+            this.setState({user: result})
+        })
+
       }
 
     render() {
         return (
+            <Container>
             <Profile 
-                username={this.state.username}
-                email={this.state.email}
-                phone={this.state.phone_number}
+                username={this.state.user ? this.state.user.username : "Loading.."}
+                email={this.state.user ? this.state.user.attributes.email : "Loading.."}
+                phone={this.state.user ? this.state.user.attributes.phone_number : "Loading.."}
             />
+            </Container>
         )
     }
 }
@@ -42,7 +37,7 @@ export default class Account extends Component {
 const Profile = (props) => (
         <div>
                 <Row>
-                <Col sm={{ span: 2, offset: 2}}>
+                <Col sm={{ span: 4, offset: 1}}>
                     <AccountInfo 
                         username={props.username}
                         email={props.email}
@@ -50,7 +45,7 @@ const Profile = (props) => (
                     />
                     </Col>
                         <MatchHistory/>
-                    <Col sm= {{ span: 6}}>
+                    <Col sm= {{ span: 8}}>
                     </Col>
                 </Row>
         </div>
@@ -59,7 +54,7 @@ const Profile = (props) => (
 const AccountInfo = (props) => (
     <div>
                     
-                        <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/ChessSet.jpg/250px-ChessSet.jpg' thumbnail />
+                        <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/ChessSet.jpg/250px-ChessSet.jpg' thumbnail fluid />
                         <ListGroup>
                             <ListGroupItem variant="flush">{props.username}</ListGroupItem>
                             <ListGroupItem>{props.email}</ListGroupItem>
@@ -70,19 +65,25 @@ const AccountInfo = (props) => (
 
 const MatchHistory = (props) => (
     <div>
-        <Table striped bordered hover>
+        <h2>Match History</h2>
+        <Table striped bordered responsive>
             <thead>
                 <tr>
                     <td>#</td>
                     <td>Variant</td>
                     <td>Opponent</td>
-                    <td>Variant</td>
                     <td>Outcome</td>
                     <td>end FEN</td>
                 </tr>
             </thead>
             <tbody>
-
+                <tr>
+                    <td>1</td>
+                    <td>AntiChess</td>
+                    <td>Magnus</td>
+                    <td>Win</td>
+                    <td>FEN here</td>
+                </tr>
             </tbody>
         </Table>
     </div>
