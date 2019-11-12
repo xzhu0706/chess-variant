fe2c92fa48d44d56f4146fb5c944697236daa0a6
Daniel Vignoles <dvignoles@gmail.com>
2019-11-11 20:45:23 -0500

    retrieve auth info for account

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import Amplify, {Auth} from 'aws-amplify'
 
 export default class Account extends Component {
+
+    async componentDidMount() {
+        const user = await Auth.currentUserInfo();
+        if (user) {
+          this.setState({
+            user: user,
+            username: user.username,
+            email: user.attributes.email,
+            email_verified: user.attributes.email_verified,
+            phone_number: user.attributes.phone_number,
+            phone_verified: user.attributes.phone_number_verified
+          });
+        }
+      }
+
     render() {
         return (
             <div>
-                Account page
+                Account Page
             </div>
         )
     }
