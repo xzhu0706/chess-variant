1880f416591f9604207ff1262d0eca78450e9d6c
Daniel Vignoles <dvignoles@gmail.com>
2019-11-13 09:38:11 -0500

    fix dialog open

--- src/pages/CreateGameDialog.js
+++ src/pages/CreateGameDialog.js
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 
 
-class CreateGameDialog extends Component{
+function CreateGameDialog() {
 
-    render(){
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
         return (
-            <Dialog open={this.props.showDialog} maxWidth='sm' fullWidth={true}>
-                <DialogTitle id="form-dialog-title">Create a game</DialogTitle>
+            <div>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                 Open form dialog
+            </Button>
+            <Dialog open={open} onClose={handleClose} maxWidth='sm' fullWidth={true}>
+                <DialogTitle id="form-dialog-title">Create a Game</DialogTitle>
                 <DialogContent>
                     <FormControl style={{minWidth: 120}}>
                         <InputLabel id="demo-simple-select-label">Variant</InputLabel>
@@ -32,9 +48,19 @@ class CreateGameDialog extends Component{
                     </FormControl>
                     
                 </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Submit
+                    </Button>
+                </DialogActions>
             </Dialog>
+            </div>
         )
-    }
+    
 }
 
 export default CreateGameDialog
