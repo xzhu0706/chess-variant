e0f80d0aa9b72f8bda540f113a4d6a2cf984f8c6
cchen008 <cchen008@citymail.cuny.edu>
2019-11-16 18:05:28 -0500

    Removed router and proptypes

--- src/components/PopularVariants.js
+++ src/components/PopularVariants.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function PopularVariant({ src, name, description }) {
   return (
-    <Router>
-      <div>
-        <h3>{name}</h3>
-        <Link to="/variants">
-          <Image src={src} alt="Variant" width={400} height={400} />
-        </Link>
-        <p>{description}</p>
-      </div>
-    </Router>
+    <div>
+      <h3>{name}</h3>
+      <Link to="/variants">
+        <Image src={src} alt="Variant" width={400} height={400} />
+      </Link>
+      <p>{description}</p>
+    </div>
   );
 }
-
-PopularVariant.propTypes = {
-  src: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
 
 export default PopularVariant;
