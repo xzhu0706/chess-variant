c7788dbd4b7ed9d3cd2b77c0c17251535daca5ed
cchen008 <cchen008@citymail.cuny.edu>
2019-11-16 17:00:02 -0500

    Added Router

--- src/components/PopularVariants.js
+++ src/components/PopularVariants.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 
 function PopularVariant({ src, name, description }) {
   return (
-    <div>
-      <h3>{name}</h3>
-      <Link to="/variants">
-        <Image src={src} alt="Variant" width={400} height={400} />
-      </Link>
-      <p>{description}</p>
-    </div>
+    <Router>
+      <div>
+        <h3>{name}</h3>
+        <Link to="/variants">
+          <Image src={src} alt="Variant" width={400} height={400} />
+        </Link>
+        <p>{description}</p>
+      </div>
+    </Router>
   );
 }
 
