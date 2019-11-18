4d4ca9fd7dd81472d87fbbbb070b4afdb745819a
Daniel Vignoles <dvignoles@gmail.com>
2019-11-18 18:48:27 -0500

    add all gamedata endgame render tests

--- src/GameData.test.js
+++ src/GameData.test.js
@@ -7,22 +7,64 @@ afterEach(cleanup)
 
 const FEN = 'FEN: rnbqkbnr/ppppp1pp/5p2/8/P7/5P2/1PPPP1PP/RNBQKBNR b KQkq a3 0 2'
 const PGN = '[SetUp "1"] [FEN "rnbqkbnr/ppppp1pp/5p2/8/8/5P2/PPPPP1PP/RNBQKBNR w KQkq - 0 2"] 2. a4'
-const checkmate_props = {turn:'w',gameResult:'checkmate',fen:FEN,pgn:PGN}
-const statlemate_props = {}
-const insufficient_props = {}
-const fift_props = {}
 
-test('render checkmate game data', () => {
+test('render fen & pgn', () => {
     const {getByText} = render(
         <GameData 
-            turn={checkmate_props.turn}
-            fen={checkmate_props.fen}
-            pgn={checkmate_props.pgn}
-            gameResult={checkmate_props.gameResult}
+            // turn={checkmate_props.turn}
+            fen={FEN}
+            pgn={PGN}
+            // gameResult={checkmate_props.gameResult}
         />,
     )
 
     expect(getByText('FEN:',{exact: false})).toHaveTextContent(FEN)
     expect(getByText('PGN:',{exact: false})).toHaveTextContent(PGN)
+    // expect(getByText('STATE:',{exact: false})).toHaveTextContent('STATE: White wins')
+})
+
+test('render checkmate', () => {
+    const {getByText} = render(
+        <GameData
+            turn={'w'}
+            gameResult={'checkmate'}
+        />,
+    )
     expect(getByText('STATE:',{exact: false})).toHaveTextContent('STATE: White wins')
+})
+
+test('render repitition', () => {
+    const {getByText} = render(
+        <GameData
+            gameResult={'repetition'}
+        />,
+    )
+    expect(getByText('STATE:',{exact: false})).toHaveTextContent('Draw (three-fold repetition)')
+})
+
+test('render stalemate', () => {
+    const {getByText} = render(
+        <GameData
+            gameResult={'stalemate'}
+        />,
+    )
+    expect(getByText('STATE:',{exact: false})).toHaveTextContent(`Draw (stalemate)`)
+})
+
+test('render insufficient', () => {
+    const {getByText} = render(
+        <GameData
+            gameResult={'insufficient'}
+        />,
+    )
+    expect(getByText('STATE:',{exact: false})).toHaveTextContent(`Draw (insufficient material)`)
+})
+
+test('render fift move rule', () => {
+    const {getByText} = render(
+        <GameData
+            gameResult={'fifty'}
+        />,
+    )
+    expect(getByText('STATE:',{exact: false})).toHaveTextContent(`Draw (fifty-move rule)`)
 })
