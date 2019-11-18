5f5c361f6afa32ae156083171ea055350e7c038b
Daouda Gueye <daoudag04@gmail.com>
2019-11-18 09:57:16 -0500

    opponent username and whose turn it is now displayed on the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import {API, graphqlOperation } from 'aws-amplify';
 import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import * as mutations from '../graphql/mutations';
 import * as subscriptions from '../graphql/subscriptions';
 import * as Games from '../Constants/GameComponentConstants';
 import * as Colors from '../Constants/Colors';
 import Chessboard from 'chessboardjsx';
 import Chess from 'chess.js';
+
+const YOUR_TURN_MESSAGE = `It's your turn!`
 
 class Game extends Component {
   constructor(props){
@@ -37,6 +41,7 @@ class Game extends Component {
       this.orientation = game.creatorOrientation === 'white'? 'black' : 'white'
       this.opponent = game.creator
     }
+    let initialFen = ''
     this.gameId = game.id
     let variant = game.variant
     switch(variant){
@@ -44,17 +49,17 @@ class Game extends Component {
         //this.game = new Antichess() waiting for Antichess.js implementation
         //this.setState({fen: Constants.ANTICHESS_FEN})
         this.game = new Chess()
-        this.setState({fen: Games.STANDARD_FEN})
+        initialFen = Games.STANDARD_FEN
         break
       case Games.STANDARD_CHESS:
         this.game = new Chess()
-        this.setState({fen: Games.STANDARD_FEN})
+        initialFen = Games.STANDARD_FEN
         break;
       default:
         this.game = new Chess()
-        this.setState({fen: Games.STANDARD_FEN})
+        initialFen = Games.STANDARD_FEN
     }
-
+    this.setState({fen: initialFen})
     this.gameUpdateSubscription = API.graphql(graphqlOperation(subscriptions.onUpdateGame),).subscribe({
       next: (gameData) => {
         let gameState = gameData.value.data.onUpdateGame
@@ -92,11 +97,10 @@ class Game extends Component {
     let newSquareStyles = {}
     if (piece !== null && piece.color === this.orientation[0]) {
       this.moveFrom = square
-      let validMoves = this.game.moves({ square: square })
+      let validMoves = this.game.moves({ square: square , verbose: true})
       newSquareStyles[square] = { backgroundColor: Colors.BOARD_HIGHLIGHT_COLOR }
       for (let i in validMoves) {
-        let move = validMoves[i];
-        move = move.length < 3 ? move : move.substring(1)
+        let move = validMoves[i].to;
         newSquareStyles[move] = {
           background: `radial-gradient(circle, ${Colors.BOARD_HIGHLIGHT_COLOR} 26%, transparent 30%)`,
           borderRadius: "50%"
@@ -113,14 +117,24 @@ class Game extends Component {
     }
     return (
       <Box display='flex' justifyContent='center'>
-        <Chessboard
-          position = {this.state.fen}
-          lightSquareStyle = {{backgroundColor: Colors.LIGHT_SQUARE}}
-          darkSquareStyle = {{backgroundColor: Colors.DARK_SQUARE}}
-          orientation = {this.orientation}
-          squareStyles = {this.state.squareStyles}
-          onSquareClick = {this.onSquareClick}
-        />
+        <Box display='flex' flexDirection='column'>
+          <Paper>
+            <Typography style={{fontFamily: 'AppleSDGothicNeo-Bold', marginLeft: '5px'}} variant="h5" component="h3">
+              You vs {this.opponent !== null? this.opponent.username : 'Anonymous'}.
+            </Typography>
+            <Typography style={{fontFamily: 'AppleSDGothicNeo-Bold', marginLeft: '5px'}}component="p">
+              {this.game !== null && this.game.turn() === this.orientation[0]? YOUR_TURN_MESSAGE : ''}
+            </Typography>
+          </Paper>
+          <Chessboard
+            position={this.state.fen}
+            lightSquareStyle={{ backgroundColor: Colors.LIGHT_SQUARE }}
+            darkSquareStyle={{ backgroundColor: Colors.DARK_SQUARE }}
+            orientation={this.orientation}
+            squareStyles={this.state.squareStyles}
+            onSquareClick={this.onSquareClick}
+          />
+        </Box>
       </Box>
     )
   }
