679ca91b451d07e02e663a234a028e3e40563691
Seyson Chen <seyson@live.com>
2019-11-19 00:28:34 -0500

    (#71) writing tests for extinction chess

--- src/chess/chess.int.test.js
+++ src/chess/chess.int.test.js
@@ -3,10 +3,11 @@
 const chessjs = require("./chess.js");
 
 /* For each variant, this file tests the functions we modified in chess/chess.js, which are
- * generate_moves() (generates a list of valid move objects)
- * in_check() (returns true if the current player's king is in check)
- * 
- * 
+ * generate_moves()         (generates a list of valid moves in the current game position)
+ * in_check()               (returns true iff the current player's king is in check - should be disabled in antichess)
+ * in_checkmate()           (returns true iff the current player's king is in checkmate - should be impossible in antichess)
+ * in_stalemate()           (returns true iff the current player has no legal moves)
+ * insufficient_material()  (returns true iff neither player can win with the pieces he/she has - should be disabled in grid chess)
  * 
  */
 
@@ -281,7 +282,7 @@ describe("Testing antichess (move generation, winning conditions, etc)", () => {
   });
 });
 
-describe("Testing grid chess (move generation)", () => {
+describe("Testing grid chess (move generation, checking and game-over conditions)", () => {
   //   a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
   //   a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
   //   a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
@@ -566,4 +567,101 @@ describe("Testing grid chess (move generation)", () => {
   test("In grid chess, draw by insufficient material cannot occur", () => {
     expect(gridGame13.insufficient_material()).toBe(false);
   });
+});
+
+describe("Testing extinction chess (winning conditions, etc.)", () => {
+  //   a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
+  //   a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
+  //   a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
+  //   a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
+  //   a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
+  //   a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
+  //   a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
+  //   a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
+  let extinctionGame = new chessjs.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQ1BNR w KQkq - 0 1", 3);
+  // +------------------------+
+  // 8 | r  n  b  q  k  b  n  r |
+  // 7 | p  p  p  p  p  p  p  p |
+  // 6 | .  .  .  .  .  .  .  . |
+  // 5 | .  .  .  .  .  .  .  . |
+  // 4 | .  .  .  .  .  .  .  . |
+  // 3 | .  .  .  .  .  .  .  . |
+  // 2 | P  P  P  P  P  P  P  P |
+  // 1 | R  N  B  Q  .  B  N  R |
+  //   +------------------------+
+  //     a  b  c  d  e  f  g  h
+  // (White's turn.)
+  test("In extinction chess, the game is not over if White has no kings at the start of the game", () => {
+    expect(extinctionGame.game_over()).toEqual(false);
+  });
+
+  let extinctionGame2 = new chessjs.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR w KQkq - 0 1", 3);
+  // +------------------------+
+  // 8 | r  n  b  q  k  b  n  r |
+  // 7 | p  p  p  p  p  p  p  p |
+  // 6 | .  .  .  .  .  .  .  . |
+  // 5 | .  .  .  .  .  .  .  . |
+  // 4 | .  .  .  .  .  .  .  . |
+  // 3 | .  .  .  .  .  .  .  . |
+  // 2 | P  P  P  P  P  P  P  P |
+  // 1 | R  N  B  .  K  B  N  R |
+  //   +------------------------+
+  //     a  b  c  d  e  f  g  h
+  // (White's turn.)
+  test("In extinction chess, the game is not over if White has no queens at the start of the game", () => {
+    expect(extinctionGame2.game_over()).toEqual(false);
+  });
+
+  let extinctionGame3 = new chessjs.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RN1QK1NR w KQkq - 0 1", 3);
+  // +------------------------+
+  // 8 | r  n  b  q  k  b  n  r |
+  // 7 | p  p  p  p  p  p  p  p |
+  // 6 | .  .  .  .  .  .  .  . |
+  // 5 | .  .  .  .  .  .  .  . |
+  // 4 | .  .  .  .  .  .  .  . |
+  // 3 | .  .  .  .  .  .  .  . |
+  // 2 | P  P  P  P  P  P  P  P |
+  // 1 | R  N  .  Q  K  .  N  R |
+  //   +------------------------+
+  //     a  b  c  d  e  f  g  h
+  // (White's turn.)
+  test("In extinction chess, the game is not over if White has no bishops at the start of the game", () => {
+    expect(extinctionGame3.game_over()).toEqual(false);
+  });
+
+  let extinctionGame4 = new chessjs.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R1BQKB1R w KQkq - 0 1", 3);
+  // +------------------------+
+  // 8 | r  n  b  q  k  b  n  r |
+  // 7 | p  p  p  p  p  p  p  p |
+  // 6 | .  .  .  .  .  .  .  . |
+  // 5 | .  .  .  .  .  .  .  . |
+  // 4 | .  .  .  .  .  .  .  . |
+  // 3 | .  .  .  .  .  .  .  . |
+  // 2 | P  P  P  P  P  P  P  P |
+  // 1 | R  .  B  Q  K  B  .  R |
+  //   +------------------------+
+  //     a  b  c  d  e  f  g  h
+  // (White's turn.)
+  test("In extinction chess, the game is not over if White has no knights at the start of the game", () => {
+    expect(extinctionGame4.game_over()).toEqual(false);
+  });
+
+  let extinctionGame5 = new chessjs.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/1NBQKBN1 w KQkq - 0 1", 3);
+  // +------------------------+
+  // 8 | r  n  b  q  k  b  n  r |
+  // 7 | p  p  p  p  p  p  p  p |
+  // 6 | .  .  .  .  .  .  .  . |
+  // 5 | .  .  .  .  .  .  .  . |
+  // 4 | .  .  .  .  .  .  .  . |
+  // 3 | .  .  .  .  .  .  .  . |
+  // 2 | P  P  P  P  P  P  P  P |
+  // 1 | .  N  B  Q  K  B  N  . |
+  //   +------------------------+
+  //     a  b  c  d  e  f  g  h
+  // (White's turn.)
+  test("In extinction chess, the game is not over if White has no rooks at the start of the game", () => {
+    expect(extinctionGame5.game_over()).toEqual(false);
+  });
+
+
 });
