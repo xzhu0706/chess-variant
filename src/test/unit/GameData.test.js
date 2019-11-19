f09b584361ac7ba0ce688ae018ab0f521a09b76b
Xiaohong Zhu <xzhu0706@gmail.com>
2019-11-19 16:28:04 -0500

    move unit tests to test folder

--- src/test/unit/GameData.test.js
+++ src/test/unit/GameData.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render, cleanup} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import GameData from './GameData.js'
+import GameData from '../../GameData.js'
 
 afterEach(cleanup)
 
