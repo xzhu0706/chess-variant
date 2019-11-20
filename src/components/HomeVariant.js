e3677d9a94464464aca427f2893c8976d7a64691
cchen008 <cchen008@citymail.cuny.edu>
2019-11-19 23:38:17 -0500

    Added some temp text

--- src/components/HomeVariant.js
+++ src/components/HomeVariant.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function HomeVariant() {
+  const headingStyle = {
+    fontFamily: 'Helvetica, Arial, sans-serif',
+    fontWeight: 'bold',
+  };
+  const bodyStyle = {
+    fontFamily: 'Lucida Bright, Lucidabright, Lucida Serif, Lucida,'
+          + 'Bitstream Charter, Bitstream Vera Serif, DejaVu Serif, Century Schoolbook L,'
+          + 'serif',
+    fontSize: '110%',
+  };
   return (
     <div>
-            a
+      <h1 className="text-center" style={headingStyle}>Chess Variants</h1>
+      <br />
+      <p style={bodyStyle}>Chess variants provides a new way of playing chess. Learn new variants and strategies and think outside of the box</p>
     </div>
   );
 }
