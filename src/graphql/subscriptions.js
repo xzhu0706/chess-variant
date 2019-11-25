f3403b8d59c1208c5d6156aa4030e24c85ba2272
Xiaohong Zhu <xzhu0706@gmail.com>
2019-11-24 20:34:19 -0500

    added user model to schema

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,78 +1,261 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateGame = `subscription OnCreateGame {
-  onCreateGame {
+export const onCreateUser = `subscription OnCreateUser {
+  onCreateUser {
     id
-    creator {
+    username
+    email
+    phoneNumber
+    pastGames {
+      items {
+        id
+      }
+      nextToken
+    }
+    points
+    skillLevel
+    rank
+  }
+}
+`;
+export const onUpdateUser = `subscription OnUpdateUser {
+  onUpdateUser {
+    id
+    username
+    email
+    phoneNumber
+    pastGames {
+      items {
+        id
+      }
+      nextToken
+    }
+    points
+    skillLevel
+    rank
+  }
+}
+`;
+export const onDeleteUser = `subscription OnDeleteUser {
+  onDeleteUser {
+    id
+    username
+    email
+    phoneNumber
+    pastGames {
+      items {
+        id
+      }
+      nextToken
+    }
+    points
+    skillLevel
+    rank
+  }
+}
+`;
+export const onCreatePlayerGameMapping = `subscription OnCreatePlayerGameMapping {
+  onCreatePlayerGameMapping {
+    id
+    game {
+      id
+      players {
+        nextToken
+      }
+      creator {
+        id
+        username
+      }
+      opponent {
+        id
+        username
+      }
+      creatorOrientation
+      time
+      variant
+      fen
+      available
+      history
+      result
+      winner
+    }
+    player {
       id
       username
+      email
+      phoneNumber
+      pastGames {
+        nextToken
+      }
       points
       skillLevel
       rank
     }
-    opponent {
+  }
+}
+`;
+export const onUpdatePlayerGameMapping = `subscription OnUpdatePlayerGameMapping {
+  onUpdatePlayerGameMapping {
+    id
+    game {
+      id
+      players {
+        nextToken
+      }
+      creator {
+        id
+        username
+      }
+      opponent {
+        id
+        username
+      }
+      creatorOrientation
+      time
+      variant
+      fen
+      available
+      history
+      result
+      winner
+    }
+    player {
       id
       username
+      email
+      phoneNumber
+      pastGames {
+        nextToken
+      }
       points
       skillLevel
       rank
+    }
+  }
+}
+`;
+export const onDeletePlayerGameMapping = `subscription OnDeletePlayerGameMapping {
+  onDeletePlayerGameMapping {
+    id
+    game {
+      id
+      players {
+        nextToken
+      }
+      creator {
+        id
+        username
+      }
+      opponent {
+        id
+        username
+      }
+      creatorOrientation
+      time
+      variant
+      fen
+      available
+      history
+      result
+      winner
+    }
+    player {
+      id
+      username
+      email
+      phoneNumber
+      pastGames {
+        nextToken
+      }
+      points
+      skillLevel
+      rank
+    }
+  }
+}
+`;
+export const onCreateGame = `subscription OnCreateGame {
+  onCreateGame {
+    id
+    players {
+      items {
+        id
+      }
+      nextToken
+    }
+    creator {
+      id
+      username
+    }
+    opponent {
+      id
+      username
     }
     creatorOrientation
     time
     variant
     fen
     available
+    history
+    result
+    winner
   }
 }
 `;
 export const onUpdateGame = `subscription OnUpdateGame {
   onUpdateGame {
     id
+    players {
+      items {
+        id
+      }
+      nextToken
+    }
     creator {
       id
       username
-      points
-      skillLevel
-      rank
     }
     opponent {
       id
       username
-      points
-      skillLevel
-      rank
     }
     creatorOrientation
     time
     variant
     fen
     available
+    history
+    result
+    winner
   }
 }
 `;
 export const onDeleteGame = `subscription OnDeleteGame {
   onDeleteGame {
     id
+    players {
+      items {
+        id
+      }
+      nextToken
+    }
     creator {
       id
       username
-      points
-      skillLevel
-      rank
     }
     opponent {
       id
       username
-      points
-      skillLevel
-      rank
     }
     creatorOrientation
     time
     variant
     fen
     available
+    history
+    result
+    winner
   }
 }
 `;
