53e0853912d73b5ebf081ee5c60cc3e14197f135
Daniel Vignoles <dvignoles@gmail.com>
2019-11-26 18:16:03 -0500

    test live site

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 const puppeteer = require('puppeteer')
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
-test('base e2e test', async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    // slowMo: 80
+describe('App Level Tests', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
-  const page = await browser.newPage();
-  await page.goto(
-    'http://chess-variant-20191104002115-hostingbucket-dev.s3-website-us-east-1.amazonaws.com/'
-  );
-});
+
+  test('base e2e test', async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      // slowMo: 80
+    });
+    const page = await browser.newPage();
+    await page.goto(
+      'http://chess-variant-20191104002115-hostingbucket-develop.s3-website-us-east-1.amazonaws.com/'
+    );
+    await page.goto
+
+    await browser.close();
+  });
+
+})
+
+
