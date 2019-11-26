11245af454dee327af145de5f9a5100607823131
cchen008 <cchen008@citymail.cuny.edu>
2019-11-25 21:31:41 -0500

    Added css to Clock.js # 83

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Countdown from 'react-countdown-now';
 
+const clockStyle = {
+  fontSize: '30px',
+};
+
 function Clock() {
   return (
-    <Countdown
-      date={Date.now() + 3600000}
-      intervalDelay={3}
-      zeroPadTime={2}
-      autoStart={false}
-      daysInHours
-    />
+    <div style={clockStyle}>
+      <Countdown
+        date={Date.now() + 3600000}
+        intervalDelay={3}
+        zeroPadTime={2}
+        autoStart={false}
+        daysInHours
+      />
+    </div>
   );
 }
 
