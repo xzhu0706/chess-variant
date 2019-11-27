f35b6a06824e8e8dd97efec6a33b5c281aad56b0
Daniel Vignoles <dvignoles@gmail.com>
2019-11-27 08:46:08 -0500

    create game e2e test

--- src/App.test.js
+++ src/App.test.js
@@ -5,22 +5,26 @@ const puppeteer = require('puppeteer')
 
 
 describe('App Level Tests', () => {
-  test('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  // test('renders without crashing', () => {
+  //   const div = document.createElement('div');
+  //   ReactDOM.render(<App />, div);
+  //   ReactDOM.unmountComponentAtNode(div);
+  // });
 
   test('base e2e test', async () => {
     const browser = await puppeteer.launch({
       headless: false,
-      // slowMo: 80
+       slowMo: 80
     });
     const page = await browser.newPage();
     await page.goto(
-      'http://chess-variant-20191104002115-hostingbucket-develop.s3-website-us-east-1.amazonaws.com/'
+      // 'http://chess-variant-20191104002115-hostingbucket-develop.s3-website-us-east-1.amazonaws.com/'
+      'http://localhost:3000/'
     );
-
+    
+    await page.click('#btncreategame');
+    await page.select('#select-variant','Antichess')
+    await page.click("#btnwhite")
     await browser.close();
   });
 
