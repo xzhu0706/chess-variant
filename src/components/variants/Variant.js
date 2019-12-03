742584b9d0845c18c36efee731dc84afba5814b0
cchen008 <cchen008@citymail.cuny.edu>
2019-12-02 23:00:01 -0500

    Fixed eslint errors in Variant.js #66

--- src/components/variants/Variant.js
+++ src/components/variants/Variant.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-const Variant = (props) => {
+const Variant = ({ title, children }) => {
   const headingStyle = {
-      fontFamily: "Helvetica, Arial, sans-serif",
-      fontWeight: "bold",
-      textAlign: "center"
-    };
-  
-    const bodyStyle = {
-      fontFamily: "Lucida Bright, Lucidabright, Lucida Serif, Lucida," +
-        "Bitstream Charter, Bitstream Vera Serif, DejaVu Serif, Century Schoolbook L," +
-        "serif",
-      fontSize: "100%",
-      marginBottom: "1rem"
-    };
+    fontFamily: 'Helvetica, Arial, sans-serif',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  };
+
+  const bodyStyle = {
+    fontFamily: 'Lucida Bright, Lucidabright, Lucida Serif, Lucida,'
+        + 'Bitstream Charter, Bitstream Vera Serif, DejaVu Serif, Century Schoolbook L,'
+        + 'serif',
+    fontSize: '100%',
+    marginBottom: '1rem',
+  };
 
   return (
     <Container>
-      <h1 style={headingStyle}>Rules of {props.title}</h1>
+      <h1 style={headingStyle}>
+        Rules of
+        {' '}
+        {title}
+      </h1>
       <Row xs={8}>
         <div>
           <div style={bodyStyle}>
-            {props.children}
+            {children}
           </div>
         </div>
       </Row>
     </Container>
   );
-}
+};
 
 export default Variant;
+
+Variant.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
