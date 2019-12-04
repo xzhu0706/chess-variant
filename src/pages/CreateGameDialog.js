b530c9016a1b7006fd7c8d5f3f7a0a7e1d30f7ed
Daniel Vignoles <dvignoles@gmail.com>
2019-12-04 10:31:33 -0500

    add user search field to creategamedialog

--- src/pages/CreateGameDialog.js
+++ src/pages/CreateGameDialog.js
@@ -5,6 +5,14 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
+import * as queries from '../graphql/queries';
+
+
+
+
 
 class CreateGameDialog extends Component {
     constructor(props) {
@@ -24,7 +32,31 @@ class CreateGameDialog extends Component {
         console.log(e.target.value);
     }
 
+    handleSearch = async (e) => {
+        // clear the search results each time the search input is updated
+        this.setState({
+          searchResults: [],
+        });
+        const input = e.target.value;
+        // start searching after 2 characters input
+        if (input.length > 2) {
+          const filter = {
+            username: {
+              contains: input,
+            },
+          };
+          const queryResult = await API.graphql(graphqlOperation(queries.listUsers, { filter }));
+          this.setState({
+            searchResults: queryResult.data.listUsers.items,
+          });
+          console.log(queryResult.data.listUsers.items);
+        }
+      }
+
     render() {
+        const {
+            searchResults
+          } = this.state;
         return (
             <Dialog open={this.props.showDialog} maxWidth="sm" fullWidth onClose={this.props.closeDialog}>
                 <DialogTitle id="form-dialog-title">Create a game</DialogTitle>
@@ -43,6 +75,24 @@ class CreateGameDialog extends Component {
                             <option value="Extinction">Extinction chess</option>
                         </select>
                         <br />
+
+                        <Autocomplete
+                            className="d-inline-block"
+                            id="search-bar"
+                            style={{ width: 250 }}
+                            getOptionLabel={(option) => option.username}
+                            noOptionsText="No user found"
+                            options={searchResults}
+                            onChange={this.linkToUser}
+                            onInputChange={this.handleSearch}
+                            renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Search a User"
+                                fullWidth
+                            />
+                            )}
+                         />
                     </FormControl>
                     <div style={{ width: '100%', marginTop: '15px' }}>
 
