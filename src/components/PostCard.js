1d5d8e74f5c61a65ceac8cab433929dfc941c91b
Daouda Gueye <daoudag04@gmail.com>
2019-12-07 13:58:02 -0500

    added DiscussionBoard component for listing user posts

---
+++ src/components/PostCard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Card, CardActions, CardContent } from '@material-ui/core';
+
+class PostCard extends Component{
+
+    render(){
+        <Card>
+            <CardContent>
+                 
+            </CardContent>
+        </Card>
+    }
+}
---
+++ src/pages/DiscussionBoard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {List} from '@material-ui/core';
+
+
+class DiscussionBoard extends Component{
+
+    render(){
+        return (
+            <List 
+            />
+        )
+    }
+}
