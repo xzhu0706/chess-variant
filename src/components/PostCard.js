d4b8c4cd53e3175b8a577d4d7c526e78713d82ad
Daouda Gueye <daoudag04@gmail.com>
2019-12-07 15:36:23 -0500

    populate PostCard from props

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
-import { Card, CardActions, CardContent } from '@material-ui/core';
+import { Card, Avatar, CardContent, Typography } from '@material-ui/core';
 
 class PostCard extends Component{
 
     render(){
-        <Card>
-            <CardContent>
-                 
-            </CardContent>
-        </Card>
-    }
+        return (
+            <Card>
+                <Typography variant='h2' color='textPrimary' component='h2'>
+                    {this.props.title}
+                </Typography>
+                <CardHeader
+                    avatar = {
+                        <Avatar>D</Avatar>
+                    } 
+                    title = {this.props.author}
+                />
+                <CardContent>
+                    <Typography variant='body2' color='textSecondary' component='p'>
+                        {this.props.content}
+                    </Typography>
+                </CardContent>
+            </Card>
+        )}
 }
