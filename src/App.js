9644d920c532aca5fc8e7aee7af7b40fd4c99864
Daouda Gueye <daoudag04@gmail.com>
2019-12-09 19:25:57 -0500

    added sidebar items

--- src/App.js
+++ src/App.js
@@ -11,43 +11,11 @@ import DiscussionBoard from './pages/DiscussionBoard';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import List from '@material-ui/icons/List';
-import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-
-import Drawer from '@material-ui/core/Drawer';
-
-
 import Pieces from './pages/Pieces';
 import './App.css';
 import NavBar from './components/NavBar';
+import Sidebar from './components/Sidebar';
 import AdminDashboard from './pages/AdminDashboard';
-
-const drawerWidth = 240;
-const classes = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: 300,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  toolbar: theme.mixins.toolbar,
-}));
 
 
 class App extends Component {
@@ -61,34 +29,8 @@ class App extends Component {
   render() {
     return (
       <Router style={{backgroundColor: 'blue'}}>
-        {/*<NavBar />*/}
-        <div className={classes.root}>
-        <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+        <NavBar />
+        <Sidebar />
         <Switch>
           <Route path="/" exact component={DiscussionBoard} />
           <Route path="/game/:id" component={Game} />
@@ -106,7 +48,6 @@ class App extends Component {
           <Route path="/pieces" component={Pieces} />
           <Route path="/admin" component={AdminDashboard} />
         </Switch>
-        </div>
       </Router>
     );
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
+import {List, ListItemAvatar, Avatar, ListItemText, ListItem} from '@material-ui/core';
+
 
 class Sidebar extends Component {
 
     render() {
         return (
-            <Box
-                display='flex' 
-                flexDirection='column' 
-                style={{width:'25%'}}
-            />
+            <List position='fixed' style={{backgroundColor: 'blue', width: '25%', height: '100%'}}>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="" src=""/>
+                    </ListItemAvatar>
+                    <ListItemText primary="Home"/>
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="" src=""/>
+                    </ListItemAvatar>
+                    <ListItemText primary="Play"/>
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="" src=""/>
+                    </ListItemAvatar>
+                    <ListItemText primary="Browse Variants"/>
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="" src=""/>
+                    </ListItemAvatar>
+                    <ListItemText primary="Create a Variant"/>
+                </ListItem>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="" src=""/>
+                    </ListItemAvatar>
+                    <ListItemText primary="Glossary of Pieces"/>
+                </ListItem>
+            </List>
         )
     }
 }
