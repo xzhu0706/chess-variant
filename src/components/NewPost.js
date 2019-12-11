564d7216cf91c2e6f99cbd3d1d6191ed5ccef78d
Daouda Gueye <daoudag04@gmail.com>
2019-12-10 20:02:12 -0500

    added button on newPostComponent for submitting new posts

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {List, Container, Box} from '@material-ui/core';
+import {List, Container, Box, Button} from '@material-ui/core';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 
@@ -7,9 +7,20 @@ class NewPost extends Component {
 
     render(){
         return(
-            <Box>
-                <TextareaAutosize aria-label="empty textarea" placeholder="Empty" />;
+            <Box style={{border: '1px solid lightGray'}} display='flex' flexDirection='column'>
+                <TextareaAutosize
+                    aria-label="empty textarea" 
+                    placeholder="Empty" 
+                    style={{width: '100%'}}
+                    rows = {5}
+                    />
+                <Box display='flex' flexDirection='row' alignItems='center' 
+                    style={{backgroundColor: 'white', height: '50px'}}>
+                    <Button variant="contained" color="primary">Submit</Button>
+                </Box>
             </Box>
         )
     }
 }
+
+export default NewPost
