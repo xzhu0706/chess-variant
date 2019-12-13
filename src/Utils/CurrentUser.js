cf32dde67b486fcd7ef90051afdae5c0083a8855
Daouda Gueye <daoudag04@gmail.com>
2019-12-13 08:19:48 -0500

    refactored getUserInfo into a separate file. It was defined and used in multiple files

---
+++ src/Utils/CurrentUser.js
@@ -0,0 +1,17 @@
+import {Auth } from 'aws-amplify';
+
+getUserInfo = async() => {
+    const currentUser = {};
+    await Auth.currentAuthenticatedUser().then((user) => {
+      currentUser.id = user.attributes.sub;
+      currentUser.username = user.username;
+    }).catch(async (e) => {
+      await Auth.currentCredentials().then((credential) => {
+        currentUser.id = credential.identityId.split(':')[1];
+        currentUser.username = 'anonymous';
+      });
+    });
+    return currentUser;
+}
+
+export default getUserInfo
