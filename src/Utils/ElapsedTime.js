962083367a88454980c6ed33f1c6d82ea9a601d6
Daouda Gueye <daoudag04@gmail.com>
2019-12-15 14:39:45 -0500

    add suport for month and year in elapsed time

--- src/Utils/ElapsedTime.js
+++ src/Utils/ElapsedTime.js
@@ -2,24 +2,22 @@ import * as Time from '../Constants/TimeConstants';
 
 /**
  * Input: A DateTime value in the following format: 
- * Output: Elapsed time since the given DateTime: Either Number of days, hours, minutes or seconds
+ * Output: Elapsed time since the given DateTime: 
+ * Either Number of years, months, days, hours, minutes or seconds
  */
 function getElapsedTime(creationDate) {
-    /**
-     * 1min = 60s = 
-     * 1hour = 60 mins = 3600s
-     * 1 day = 24h = 86400s
-     */
     let date = new Date(creationDate)
     let now = new Date()
-    
+
     //timeDiff is converted to seconds from milliseconds
     let timeDiff = parseInt((now-date)/1000)
     let elapsedTime;
-    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_DAY)) > 0) return elapsedTime + 'd'
-    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_AN_HOUR)) > 0) return elapsedTime + 'h'
-    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_MINUTE)) > 0) return elapsedTime + 'm'
-    return timeDiff + 's'
+    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_YEAR)) > 0) return elapsedTime + Time.YEAR_REPRESENTATION
+    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_MONTH)) > 0) return elapsedTime + Time.MONTH_REPRESENTATION
+    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_DAY)) > 0) return elapsedTime + Time.DAY_REPRESENTATION
+    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_AN_HOUR)) > 0) return elapsedTime + Time.HOURS_REPRESENTATION
+    if((elapsedTime = parseInt(timeDiff/Time.SECONDS_IN_A_MINUTE)) > 0) return elapsedTime + Time.MINUTES_REPRESENTATION
+    return Time.SECONDS_REPRESENTATION
 }
 
 export default getElapsedTime
