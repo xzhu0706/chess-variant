5b2da84a54ae7425990a4e0ca246bf3bca422c57
Daouda Gueye <daoudag04@gmail.com>
2019-12-15 13:35:07 -0500

    added code for computing the right time interval to use for updating the elapsed time shown on the posts

--- src/Constants/TimeConstants.js
+++ src/Constants/TimeConstants.js
@@ -1,3 +1,7 @@
 export const SECONDS_IN_A_MINUTE = 60
 export const SECONDS_IN_AN_HOUR = 3600
 export const SECONDS_IN_A_DAY = 86400
+
+export const MILLISECONDS_IN_A_MINUTE = 60000
+export const MILLISECONDS_IN_AN_HOUR = 3e6
+export const MILLISECONDS_IN_A_DAY = 8e7
